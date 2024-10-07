582a34eefac5960ec2a195c091d32c3eb3b0daac
UNKNOWCALLBACK_TEAM <147795365+UNKNOWCALLBACK@users.noreply.github.com>
2024-10-07 22:00:18 +0700

    Update config.ItemRecorey.lua

--- config/config.ItemRecorey.lua
+++ config/config.ItemRecorey.lua
@@ -1,3 +1,10 @@
 Config = {} or nil
 
 Config.tokentogetitem = "Guhjdbfhdsvfgh" --ตั้งให้เดายากเพื่อป้องกันการเสกไอเทม
+Config.ItemStart = {
+    {
+        ItemName = "newplayerboxset", --ไอเท็ม
+        ItemCount = 1, --จำนวน
+        Percent = 100  --เปอร์เซนต์กการดรอปไอเท็ม
+    },
+}
